652d7f318754d2e9c5b54354783c87ced54de222
Hanna Yakush <hanna.yakush@jetbrains.com>
2024-11-08 14:38:27 +0100

    Revert "Refactor logic logic, add Post model"
    This reverts commit 3b4c1e39e97e80da1e661b35c0a20a3bac7a08f7.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,94 +2,46 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'invited_by',
-        'invitation_code',
-        'email_verified_at',
-        'banned_at',
-        'remember_token',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'banned_at' => 'datetime',
-    ];
-
-    public function invitedBy(): BelongsTo
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->belongsTo(User::class, 'invited_by', 'id');
-    }
-
-    public function postVisits(): HasMany
-    {
-        return $this->hasMany(UserPostVisit::class);
-    }
-
-    public function addPostVisit(Post $post): void
-    {
-        UserPostVisit::updateOrCreate([
-            'user_id' => $this->id,
-            'post_id' => $post->id,
-        ], [
-            'visited_at' => now(),
-        ]);
-    }
-
-    public function lastSeenAt(Post|int $postId): ?Carbon
-    {
-        if ($postId instanceof Post) {
-            $postId = $postId->id;
-        }
-
-        $latestPostVisit = $this->postVisits
-            ->first(fn (UserPostVisit $postVisit) => $postVisit->post_id === $postId);
-
-        return $latestPostVisit?->visited_at;
-    }
-
-    public function hasUnreadComments(Post $post): bool
-    {
-        if (! $post->last_comment_at) {
-            return false;
-        }
-
-        $lastSeenAt = $this->lastSeenAt($post);
-
-        if (! $lastSeenAt) {
-            return true;
-        }
-
-        return $lastSeenAt->lt($post->last_comment_at);
-    }
-
-    public function hasSeenComment(PostComment $comment, ?Carbon $lastSeenAt = null): bool
-    {
-        $lastSeenAt ??= $this->lastSeenAt($comment->post_id);
-
-        if (! $lastSeenAt) {
-            return true;
-        }
-
-        return $lastSeenAt->gte($comment->created_at);
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
